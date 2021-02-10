b6d5b6798e8f7b6e12423826b000b54ebb01038f
zishino17 <45171471+zishino17@users.noreply.github.com>
2021-02-10 04:50:10 -0500

    Add files via upload

---
+++ haixing.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+
+
+$("#star3").click(function(){
+    	$("#we").show();
+    	});
+
+
+
+$("#we").click(function(){
+          	$("#hello").hide();
+          	});
+            $("#haibian").click(function(){
+                      	$("#hello").hide();
+                      	});
+
+              $("#mn17i").click(function(){
+                                    $("#liujiaolou").show();
+                                    });
+              $("#liujiaolou").click(function(){
+                                              	$("#liujiaolou").hide();
+                                              	});
+})
